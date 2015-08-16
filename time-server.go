764a034b9ff9a40d86b585c51cfc6a1fa3c5adc3
Jeremy Greer <jeremy.greer.atl@gmail.com>
2015-08-15 22:25:34 -0400

    initial commit

---
+++ time-server.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+  "io"
+  "net/http"
+  "log"
+  "time"
+)
+
+func server(responseWriter http.ResponseWriter, req *http.Request) {
+  timeString := time.Now().Format(time.Stamp)
+  io.WriteString(responseWriter, timeString + "\n")
+}
+
+func main() {
+  const PORT = ":8080"
+
+  log.Print("listening at http://localhost" + PORT)
+
+  http.HandleFunc("/time", server)
+  err := http.ListenAndServe(PORT, nil)
+
+  if err != nil {
+    log.Fatal("unable to start server: ", err)
+  }
+}
